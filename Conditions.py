4626979ac64b0abcfc897bcb3c0aaa771929b483
Sudikshya Bhattarai <sudikshya.bhattarai@tuni.fi>
2025-11-25 13:34:07 +0200

    learning conditional operators in python

---
+++ Conditions.py
@@ -0,0 +1,16 @@
+age = 24
+name = "sudikshya"
+x=4
+
+if age == 24 and name == "sudikshya":
+    print("Hello Sudikshya, you are 24 years old.")
+
+if age == 24:
+    print("Hi! I am Sudikshya.")
+elif name != "sudikshya":
+    print("Hello, I am not Sudikshya.")
+else:
+    print("Hello, I am Sudikshya and I am 24 years old.")
+
+
+
